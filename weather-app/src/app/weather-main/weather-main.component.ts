575dbe997d0054fe9a495d9ccd77e5407abfb8a0
Anita Tapodi <tapodi.anita100@gmail.com>
2022-01-26 12:28:37 +0100

    weather main component and style

---
+++ weather-app/src/app/weather-main/weather-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-main',
+  templateUrl: './weather-main.component.html',
+  styleUrls: ['./weather-main.component.scss']
+})
+export class WeatherMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
